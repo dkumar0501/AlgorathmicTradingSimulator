e7329be2a2f9b4b8ed6fd6e53982e196cfe038dd
D Kumar <deepkumar0501@gmail.com>
2025-04-18 00:44:36 +0530

    Update testSuite.ml

--- testSuite.ml
+++ testSuite.ml
@@ -19,6 +19,7 @@ let add_test_case_to_suite tc = test_suite := tc::!test_suite
 let print_results = 
 	fun _ -> Printf.printf "\027[32m%d tests passed/%d tests failed/%d exceptions in %fs\n" 
 												 !numPassed !numFailed !numExcept (Time.to_float (Time.now ()) -. !start_time)
+		| Some v -> v
 
 let handle_test_result test_id printer act exp t = 
 	try print_string ((if t 
