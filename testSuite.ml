f22f47701cb048d19e78533d4157a46f5a7d1ff2
D Kumar <deepkumar0501@gmail.com>
2025-08-14 22:18:11 +0530

    Update testSuite.ml

--- testSuite.ml
+++ testSuite.ml
@@ -8,7 +8,7 @@ let first = ref false
 let numPassed = ref 0
 let numFailed = ref 0
 let numExcept = ref 0
-let test_suite = ref []
+let test_suite = ref [] 
  
 let get_option = function None -> raise EmptyRef 
 
