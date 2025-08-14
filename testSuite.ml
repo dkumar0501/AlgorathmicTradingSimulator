18c1fb256d2e29727ff8946b53d8db719609073a
D Kumar <deepkumar0501@gmail.com>
2025-08-14 22:18:45 +0530

    Update testSuite.ml

--- testSuite.ml
+++ testSuite.ml
@@ -6,7 +6,7 @@ exception EmptyRef;;
 let start_time = ref 0.  
 let first = ref false
 let numPassed = ref 0 
-let numFailed = ref 0
+let numFailed = ref 0 
 let numExcept = ref 0
 let test_suite = ref [] 
  
