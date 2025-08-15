0b568c200086924e7f1fb2b072093aefe6c7f4aa
D Kumar <deepkumar0501@gmail.com>
2025-08-15 14:49:42 +0530

    Update analyzer.mli

--- analyzer.mli
+++ analyzer.mli
@@ -2,7 +2,7 @@ type position = [ `Long | `Stay | `Short ]
 type action = [ `Buy | `None | `Sell ]
 type portfolio = (string * float) list
   
-(** Compare two values to determine long or short signal *)   
+(** Compare two values to determine long or short signal *)    
 val mov_avg_comp : float -> float -> [> `Long | `Short ]    
   
 (** Convert a position to a corresponding action *) 
