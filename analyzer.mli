7f139a9a27578bd162d479a1d4eebaae4d27ed3d
D Kumar <deepkumar0501@gmail.com>
2025-08-15 14:53:20 +0530

    Update analyzer.mli

--- analyzer.mli
+++ analyzer.mli
@@ -4,7 +4,7 @@ type portfolio = (string * float) list
     
 (** Compare two values to determine long or short signal *)     
 val mov_avg_comp : float -> float -> [> `Long | `Short ]     
-   
+    
 (** Convert a position to a corresponding action *) 
 val pos_to_act : position -> action  
  
