b5af6728a96318ddd8372f31f7c50d5e0dca773d
D Kumar <deepkumar0501@gmail.com>
2025-08-18 00:05:25 +0530

    Update analyze_companies.ml

--- analyze_companies.ml
+++ analyze_companies.ml
@@ -3,7 +3,7 @@ open Async
 open Analyzer
  
 module Mov = MovingAverageAnalyzer(Scraper.BasicScraper)
-      
+     
 let print_result ~year ~ticker ~profit ~investment = 
   printf "%d [%s]: $%.2f on $%.2f @ %.2f%% return\n"
     year ticker profit investment ((profit /. investment) *. 100.)
