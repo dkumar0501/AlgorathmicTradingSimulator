444cb6f4253f5fc8159a8c10c3a3dc0fd5c1ad26
D Kumar <deepkumar0501@gmail.com>
2025-11-05 13:55:18 +0530

    Update analyzer.ml

--- analyzer.ml
+++ analyzer.ml
@@ -9,7 +9,7 @@ type portfolio = (string * float) list
 let period = 200  
 let debug = false
   
-let portfolio_to_string p = 
+let portfolio_to_string p =  
   List.fold p ~init:"" ~f:(fun acc (s, f) -> s ^ ": " ^ Float.to_string f ^ ", " ^ acc)  
 
 let mov_avg_comp a b = if a < b then `Short else `Long
